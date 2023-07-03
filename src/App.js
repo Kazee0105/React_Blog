b7e33d0362044513674d80c95a7031991b2bc214
Kazee PC <masuda144@hotmail.com>
2023-07-03 23:37:56 +0900

    Lesson 4

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = 'Welcome to the Kazee\'s blog';
+  const likes = 50; //numbers strings arrays work. Booleans do not.
+  const person = {name: 'yoshi',age: 30}
+  const link = "https://github.com/Kazee0105"
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>{title}</h1>
+        <p>Liked { likes }</p>
+
+        <p>{10}</p>
+        <p>{'Hello ninjas'}</p>
+        <p>{[1,2,3,4,5]}</p>
+        <p>{ Math.random() * 10 }</p>
+
+        <a href={link}>Kazee</a>
+      </div>
     </div>
   );
 }
