96e316e55ebae928c88d8a14407aa535bc011a04
Kazee PC <masuda144@hotmail.com>
2023-07-03 23:47:59 +0900

    added a nav and home component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import './App.css';
+import Navbar from './navbar';
+import Home from './Home';
 
 function App() {
-  const title = 'Welcome to the Kazee\'s blog';
-  const likes = 50; //numbers strings arrays work. Booleans do not.
-  const person = {name: 'yoshi',age: 30}
-  const link = "https://github.com/Kazee0105"
 
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{title}</h1>
-        <p>Liked { likes }</p>
-
-        <p>{10}</p>
-        <p>{'Hello ninjas'}</p>
-        <p>{[1,2,3,4,5]}</p>
-        <p>{ Math.random() * 10 }</p>
-
-        <a href={link}>Kazee</a>
+        <Home />
       </div>
     </div>
   );
